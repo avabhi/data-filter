bd7ed1025ab8bed54c267b54fa5124224c2bfaa6
Abhinav <vabhinav368@gmail.com>
2024-05-16 16:31:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { data } from "../src/constants/constant";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [formattedData, setFormattedData] = useState([]);
+
+  useEffect(() => {
+    let combinedData = [];
+
+    for (let i = 0; i < data.length; i++) {
+      let bucket = data[i].bucket;
+      let emission =
+        (data[i].emissions ? data[i].emissions : 0) +
+        (data[i].transportEmissions ? data[i].transportEmissions : 0);
+
+      let existingObject = combinedData.find((obj) => obj.bucket === bucket);
+
+      if (existingObject) {
+        existingObject.emissions += emission;
+      } else {
+        combinedData.push({ ...data[i], emissions: emission });
+      }
+    }
+    setFormattedData(combinedData);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="data-table">
+        <thead>
+          <tr>
+            <th>Bucket</th>
+            <th>Emissions</th>
+            <th>Metric</th>
+          </tr>
+        </thead>
+        <tbody>
+          {formattedData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.bucket}</td>
+              <td>{item.emissions}</td>
+              <td>{item.metric}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/constants/constant.js
@@ -0,0 +1,553 @@
+export const data = [
+  {
+    emissions: null,
+
+    bucket: "2023-05-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-05-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-06-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-06-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-07-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-07-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-08-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-08-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-09-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-09-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-10-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-10-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-11-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-11-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2023-12-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: 1152.43128,
+
+    bucket: "2023-12-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: 0.11231999999999999,
+
+    bucket: "2024-01-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2024-01-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2024-02-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2024-02-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2024-03-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: 364848.12,
+
+    bucket: "2024-03-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2024-04-01T00:00:00.000Z",
+
+    metric: "expense",
+  },
+
+  {
+    emissions: null,
+
+    bucket: "2024-04-01T00:00:00.000Z",
+
+    metric: "food",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-05-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-06-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-07-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-08-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-09-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-10-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-11-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-12-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-01-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-02-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-03-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 6207.247294,
+
+    transportEmissions: 61.457893999999996,
+
+    bucket: "2024-04-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-05-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-06-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-07-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-08-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 956.7429290680001,
+
+    transportEmissions: 9.472702267999999,
+
+    bucket: "2023-09-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-10-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-11-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-12-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-01-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-02-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-03-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-04-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-05-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-06-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-07-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-08-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-09-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-10-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-11-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2023-12-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-01-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-02-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 0,
+
+    transportEmissions: null,
+
+    bucket: "2024-03-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+
+  {
+    emissions: 9349864.725968,
+
+    transportEmissions: 359610.18176799995,
+
+    bucket: "2024-04-01T00:00:00.000Z",
+
+    metric: "embodiedCarbon",
+  },
+];
